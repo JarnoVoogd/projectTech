fc3914594a51fc0c020d0360a1e78610b41ea627
Jarno <jarnovoogd@gmail.com>
2023-02-23 15:20:04 +0100

    pc conficuratie

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("hallo")
+const express = require('express')
+const app = express()
+const port = 3000 
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.send('<h1> Welkom bij deze matching app</h1>')
+})
+
+app.get('/about', (req, res) => {
+    res.render('views\about')
+})
+
+app.listen(port, () => {
+    console.log(`deze app luistert naar port ${port}`)
+})
