8fb6f2b14d80852d1ab128a0a00cfc62067123db
Jarno <jarnovoogd@gmail.com>
2023-03-03 16:41:46 +0100

    ejs uitproberen

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const serveStatic = require('serve-static')
 const port = 3000 
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.send('<h1> Welkom bij deze matching app</h1>')
+    // res.send('<h1> Welkom bij deze matching app</h1>')
+    res.render('index', { title: "hoi", message: 'hallo'})
 })
 
+// app.use('/test', serveStatic(path.join('about.html', "/views")))
+
+// app.use('/test', (req, res) => {
+//     res.serveStatic(path.join('about.html', "/views"))
+// })
+
 app.get('/about', (req, res) => {
-    res.render('views\about')
+    res.sendFile(__dirname+"/views/about.html", __dirname+"/views/about.css")
+    
 })
 
 app.listen(port, () => {
