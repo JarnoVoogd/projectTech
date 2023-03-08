3748be4de852ec8d6f02e9e769506faa7a608d0f
Jarno <jarnovoogd@gmail.com>
2023-03-09 00:40:26 +0100

    REFACTOR - ophalen van juiste profiel adhv route

--- server.js
+++ server.js
@@ -16,53 +16,85 @@ app.get('/about', (req, res) => {
     res.render('pages/about')
 })
 
+const profielen = [
+    {
+        "id" : "id1", 
+        "name" : "Jarno Voogd", 
+        "age" : "22", 
+        "about" : "Hi, I'm 22 years old and I love techno. Im currently still in school studying webdevelopment. I would like to meet new people that share my love for techno.",
+        "image" : "/images/imageJarno.jpg", 
+        "age" : "Red Armor, Baby", 
+    },
+    {
+        "id" : "id2", 
+        "name" : "Sven Zoutman", 
+        "age" : "22", 
+        "about" : "I like turtles.",
+        "image" : "/images/imageSven.jpeg", 
+        "age" : "Both, Samantha", 
+    },
+    {
+        "id" : "id3", 
+        "name" : "Ronan Doeleman", 
+        "age" : "22", 
+        "about" : "Ik houd van loodgieten en koud douchen.",
+        "image" : "/images/imageRonan.jpg", 
+        "age" : "Fashion, Drip too hard", 
+    },
+    ]
 
-const id1 = {
-    name : "Jarno Voogd",
-    age : "22",
-    about : "Hi, I'm 22 years old and I love Techno. I'm currently still in school studying webdevelopment. I would like to meet new people that share my love for techno.",
-    image : '/images/imageJarno.jpg',
-    favSongs : "Red Armor, Baby"
+// const id1 = {
+//     name : "Jarno Voogd",
+//     age : "22",
+//     about : "Hi, I'm 22 years old and I love Techno. I'm currently still in school studying webdevelopment. I would like to meet new people that share my love for techno.",
+//     image : '/images/imageJarno.jpg',
+//     favSongs : "Red Armor, Baby"
     
-}
+// }
 
-const id2 = {
-    name : "Sven Zoutman",
-    age : "22",
-    about : "I like turtles",
-    image : '/images/imageSven.jpeg',
-    favSongs : "Both, Samantha"
-}
+// const id2 = {
+//     name : "Sven Zoutman",
+//     age : "22",
+//     about : "I like turtles",
+//     image : '/images/imageSven.jpeg',
+//     favSongs : "Both, Samantha"
+// }
 
-const id3 = {
-    name : "Ronan Doeleman",
-    age : "22",
-    about : "Ik houd van loodgieten en koud douchen",
-    image : '/images/imageRonan.jpg',
-    favSongs : "Fashion, Drip too hard"
-} 
+// const id3 = {
+//     name : "Ronan Doeleman",
+//     age : "22",
+//     about : "Ik houd van loodgieten en koud douchen",
+//     image : '/images/imageRonan.jpg',
+//     favSongs : "Fashion, Drip too hard"
+// } 
 
 
 app.get('/myprofile/:user', (req, res) => {
     let userNameRoute = req.params.user
-    console.log(`dit is de pagina van ${req.params.user}`)
+    let gekozenProfiel = profielen.find(profiel => profiel.id === userNameRoute)
+    console.log(`dit is de pagina van ${gekozenProfiel.name} `)
+    console.log(gekozenProfiel)
 
-    if (userNameRoute === "id1"){
-        console.log("hoi jarno")
-        res.render('pages/myprofile', {
-            user : id1
-        })
-    } else if (userNameRoute === "id2"){
-        console.log("hoi sven")
-        res.render('pages/myprofile' , {
-            user: id2
-        })
-    } else if (userNameRoute === "id3"){
-        console.log("hoi ronan")
-        res.render('pages/myprofile' , {
-            user: id3
-        })
-}})
+    res.render('pages/myprofile', {
+        user : gekozenProfiel
+    })
+
+    // if (userNameRoute === "id1"){
+    //     console.log("hoi jarno")
+    //     res.render('pages/myprofile', {
+    //         user : gekozenProfiel
+    //     })
+    // } else if (userNameRoute === "id2"){
+    //     console.log("hoi sven")
+    //     res.render('pages/myprofile' , {
+    //         user: gekozenProfiel
+    //     })
+    // } else if (userNameRoute === "id3"){
+    //     console.log("hoi ronan")
+    //     res.render('pages/myprofile' , {
+    //         user: id3
+    //     })
+})
 
 
 
