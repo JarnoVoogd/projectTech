bdf22db6a5ab7f08d20261ab4dbf61fb9cd880a1
Jarno <jarnovoogd@gmail.com>
2023-03-09 03:20:43 +0100

    REFACTOR : profielen in een apart bestand, 2 aparte bestanden nodig ivm problemen Node vs Browser

---
+++ public/scripts/profilesNode.js
@@ -0,0 +1,28 @@
+const profiles = [
+    {
+        "id" : "id1", 
+        "name" : "Jarno Voogd", 
+        "age" : "22", 
+        "about" : "Hi, I'm 22 years old and I love techno. Im currently still in school studying webdevelopment. I would like to meet new people that share my love for techno.",
+        "image" : "/images/imageJarno.jpg", 
+        "age" : "Red Armor, Baby", 
+    },
+    {
+        "id" : "id2", 
+        "name" : "Sven Zoutman", 
+        "age" : "22", 
+        "about" : "I like turtles.",
+        "image" : "/images/imageSven.jpeg", 
+        "age" : "Both, Samantha", 
+    },
+    {
+        "id" : "id3", 
+        "name" : "Ronan Doeleman", 
+        "age" : "22", 
+        "about" : "Ik houd van loodgieten en koud douchen.",
+        "image" : "/images/imageRonan.jpg", 
+        "age" : "Fashion, Drip too hard", 
+    },
+]
+
+module.exports = profiles;
